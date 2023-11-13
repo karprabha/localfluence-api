5d3a7476be6cf38c3fadcdc0e4a0bf46b3b14aa3
Prabhakar Yadav <developer.prabhakaryadav@gmail.com>
2023-11-13 23:53:02 +0530

    add populate-db script

---
+++ db-scripts/v1/populatedb.ts
@@ -0,0 +1,51 @@
+#!/usr/bin/env node
+
+import bcrypt from "bcryptjs";
+import mongoose from "mongoose";
+
+import { User } from "../../src/api/v1/models";
+
+const users = [];
+
+const userArgs = process.argv.slice(2);
+const mongoDB = userArgs[0];
+
+mongoose.set("strictQuery", false);
+
+async function userCreate(
+    first_name: string,
+    family_name: string,
+    username: string,
+    original_password: string,
+    membership_status: string,
+) {
+    const password = await bcrypt.hash(original_password, 10);
+    const user = new User({
+        first_name,
+        family_name,
+        username,
+        password,
+        membership_status,
+    });
+
+    await user.save();
+    users.push(user);
+    console.log(`Added user: ${username}`);
+}
+
+async function createData() {
+    try {
+        console.log("Connecting to MongoDB...");
+        await mongoose.connect(mongoDB);
+
+        await userCreate("John", "Doe", "johndoe", "password123", "admin");
+        await userCreate("Jane", "Smith", "janesmith", "securepwd", "user");
+    } catch (error) {
+        console.error("Error populating the database:", error);
+    } finally {
+        console.log("Closing MongoDB connection...");
+        mongoose.connection.close();
+    }
+}
+
+createData();
